6b73de9fea31b7a5176601d7f19370291ba4e130
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2016-05-22 21:42:18 -0700

    Make transpiler test remove files if they already exist

--- tests/test_transpiler.py
+++ tests/test_transpiler.py
@@ -6,12 +6,24 @@ import transpiler
 class TestTranspiler:
 
     def test_transpiler_creates_files_without_format(self):
+        try:
+            os.remove("/tmp/auto_functions.cpp")
+            os.remove("/tmp/auto_functions.h")
+        except FileNotFoundError:
+            pass
+
         transpiler.main(["--output-dir", "/tmp"])
 
         assert os.path.isfile("/tmp/auto_functions.cpp")
         assert os.path.isfile("/tmp/auto_functions.h")
 
     def test_transpiler_creates_files_with_format(self):
+        try:
+            os.remove("/tmp/auto_functions.cpp")
+            os.remove("/tmp/auto_functions.h")
+        except FileNotFoundError:
+            pass
+
         transpiler.main(["--format", "--output-dir", "/tmp"])
 
         assert os.path.isfile("/tmp/auto_functions.cpp")
